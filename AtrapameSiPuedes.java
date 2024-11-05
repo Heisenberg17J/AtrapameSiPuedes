79057e622aeac1f16cb809e246e576dd036b6398
Heisenberg17J <hejembert@gmail.com>
2024-11-05 08:03:35 -0500

    modified:   AtrapameSiPuedes.class 	modified:   AtrapameSiPuedes.java

--- AtrapameSiPuedes.java
+++ AtrapameSiPuedes.java
@@ -10,7 +10,7 @@ public class AtrapameSiPuedes{
 
         ConfigurarVentana();
 
-        timer = new Timer(125, e -> { // Movimientos fluidos
+        timer = new Timer(135, e -> { // Movimientos fluidos
             mapa.actualizar(); // Llama al método para actualizar la posición de Pacman
             mapa.repaint(); // Vuelve a dibujar el mapa y Pacman
             if(mapa.getGameOver()){
@@ -38,7 +38,13 @@ public class AtrapameSiPuedes{
     //Acabar el juego
     public void mostrarVentanaPerdiste() {
         timer.stop();
-        JOptionPane.showMessageDialog(null, "Perdiste", "Juego terminado", JOptionPane.INFORMATION_MESSAGE);
-        System.exit(0); 
+
+        int option = JOptionPane.showConfirmDialog(null, "¿Quieres intentarlo de nuevo", "intentar",JOptionPane.YES_NO_OPTION);
+
+        if (option == JOptionPane.YES_OPTION){
+            new AtrapameSiPuedes();
+        }else{
+            System.exit(0);
+        }
     }
 }
